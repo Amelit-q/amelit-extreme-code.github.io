895e6e1ba6c8235d81ab8b7b99d818bf63659129
kolosovpetro <kolosovp94@gmail.com>
2021-07-11 13:42:30 +0000

    Deploying to master from  @ 91841c196ec7e1dd56c84cfd1a87b80df36e5497 🚀

--- static/js/main.3c0e27b7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmango-ui"]=this["webpackJsonpmango-ui"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var s=n(0),i=n.n(s),c=n(3),r=n.n(c),o=(n(8),n(1));var a=function(){return Object(o.jsx)("div",{className:"App",children:"this is the first showed message"})};r.a.render(Object(o.jsx)(i.a.StrictMode,{children:Object(o.jsx)(a,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.3c0e27b7.chunk.js.map
